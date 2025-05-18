9733c5db7c4aa595e9f0f43817a0e3757cf84104
Aswin <aswinpash666@gmail.com>
2025-05-18 11:07:01 +0530

    fix: Remove unused fetchPersistedCart thunk and clean up Signin and cart-slice components

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
 
 import quickViewReducer from "./features/quickView-slice";
 import cartReducer from "./features/cart-slice";
@@ -7,20 +6,22 @@ import wishlistReducer from "./features/wishlist-slice";
 import productDetailsReducer from "./features/product-details";
 import authReducer from "./features/auth-slice";
 
+
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+
 export const store = configureStore({
   reducer: {
     quickViewReducer,
     cartReducer,
     wishlistReducer,
     productDetailsReducer,
-    authReducer,
+    authReducer
+    
+    
   },
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-// Use these hooks throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
