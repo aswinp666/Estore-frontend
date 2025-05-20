8ad8fe59350efa6e5a4ccbfe95959e06a528f604
Aswin <aswinpash666@gmail.com>
2025-05-20 17:56:44 +0530

    refactor: Clean up layout component; remove commented QuickViewModal import and streamline user loading logic

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,29 +4,37 @@ import "../css/euclid-circular-a-font.css";
 import "../css/style.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-
 import { ModalProvider } from "../context/QuickViewModalContext";
 import { CartModalProvider } from "../context/CartSidebarModalContext";
 import { ReduxProvider } from "@/redux/provider";
-// import QuickViewModal from "@/components/Common/QuickViewModal";
 import CartSidebarModal from "@/components/Common/CartSidebarModal";
 import { PreviewSliderProvider } from "../context/PreviewSliderContext";
 import PreviewSliderModal from "@/components/Common/PreviewSlider";
-
 import ScrollToTop from "@/components/Common/ScrollToTop";
 import PreLoader from "@/components/Common/PreLoader";
 import CartLoader from "../../components/CartLoader";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const [loading, setLoading] = useState<boolean>(true);
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/redux/store";
+import { setUser } from "@/redux/features/auth-slice";
+import { fetchUserCart } from "@/redux/features/cart-slice";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
   }, []);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      const parsedUser = JSON.parse(user);
+      dispatch(setUser(parsedUser));
+      dispatch(fetchUserCart());
+    }
+  }, [dispatch]);
 
   return (
     <html lang="en" suppressHydrationWarning={true}>
@@ -41,7 +49,6 @@ export default function RootLayout({
                   <>
                     <Header />
                     {children}
-                    {/* <QuickViewModal /> */}
                     <CartSidebarModal />
                     <PreviewSliderModal />
                   </>
