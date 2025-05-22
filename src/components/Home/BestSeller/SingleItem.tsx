e91db5f95052e1b99e73994853197c637b4b045e
Aswin <aswinpash666@gmail.com>
2025-05-22 13:31:16 +0530

    refactor: Map _id to productId in handleAddToCart for consistency

--- src/components/Home/BestSeller/SingleItem.tsx
+++ src/components/Home/BestSeller/SingleItem.tsx
@@ -20,14 +20,16 @@ const SingleItem = ({ item }: { item: Product }) => {
   };
 
   // add to cart
-  const handleAddToCart = () => {
-    dispatch(
-      addItemToCart({
-        ...item,
-        quantity: 1,
-      })
-    );
-  };
+const handleAddToCart = () => {
+  dispatch(
+    addItemToCart({
+      ...item,
+      productId: item._id, // add this line to map _id to productId
+      quantity: 1,
+    })
+  );
+};
+
 
   const handleItemToWishList = () => {
     dispatch(
